b6058d62fbdb923b28477928def98d744e1e0836
Tsedal Lombebo <Brightlink01@gmail.com>
2022-08-16 12:09:22 +0000

    commitng the task 3

--- sort.h
+++ sort.h
@@ -40,6 +40,7 @@ void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+void lomuto_scheme(int *array, int low, int high, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void sort_deck(deck_node_t **deck);
